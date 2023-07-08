1f17db08ee494e0657f0b25f8f5d514aaaf0af6d
ankitisgood <111147992+ankitisgood@users.noreply.github.com>
2023-07-08 23:16:23 +0530

    Create JSTest.js

---
+++ JSTest.js
@@ -0,0 +1,53 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const MFTs = []
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_name, _eyecolor, _shirtType, _bling) {
+   const MFT ={
+      "name": _name,
+      "eyecolor": _eyecolor,
+      "shirtType": _shirtType,
+      "bling": _bling
+   }
+   MFTs.push(MFT);
+   console.log("Minted : " + _name);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+   for(let i=0; i<MFTs.length; i++){
+      console.log("\nID: \t\t" + (i+1));
+      console.log("name: \t\t" + MFTs[i].name);
+      console.log("eyecolor: \t" + MFTs[i].eyecolor);
+      console.log("shirtType: \t" + MFTs[i].shirtType);
+      console.log("bling: \t\t" + MFTs[i].bling);
+   }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+  console.log("\n" + MFTs.length);
+}
+
+// call your functions below this line
+mintNFT("Aditya", "Blue", "Hoodie", "Gold Chain");
+mintNFT("Aman", "Blue", "Hoodie", "Gold Chain");
+mintNFT("Ankit", "Blue", "Hoodie", "Gold Chain");
+mintNFT("Ayush", "Blue", "Hoodie", "Gold Chain");
+
+listNFTs();
+getTotalSupply();
+ 
